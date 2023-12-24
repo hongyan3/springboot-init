67df4687c5c6f33a3937d8f354a8f3bc021141eb
xiyuan <xiyuan230@qq.com>
2023-12-24 19:07:48 +0800

    fix: options request failed

--- src/main/java/com/xiyuan/project/interceptor/LoginInterceptor.java
+++ src/main/java/com/xiyuan/project/interceptor/LoginInterceptor.java
@@ -3,6 +3,7 @@ package com.xiyuan.project.interceptor;
 import com.xiyuan.project.common.ErrorCode;
 import com.xiyuan.project.constant.UserConstant;
 import com.xiyuan.project.exception.BusinessException;
+import com.xiyuan.project.model.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +13,12 @@ import javax.servlet.http.HttpSession;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HttpSession session = request.getSession();
-        if (session == null || session.getAttribute(UserConstant.USER_LOGIN_STATE) == null) {
+        if (request.getMethod().equals("OPTIONS")){
+            return true;
+        }
+        Object userObj = request.getSession().getAttribute(UserConstant.USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
             throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
         }
         return true;
